248691f05b5588038a0796b9c3c10d10eec27526
sankalptupapara <sankalprupapara586@gmail.com>
2025-04-24 13:45:31 +0530

    Chapter-4

---
+++ Chapter-4/LEBWORK-4/4.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main(){
+	int a=22 , b=26 , c=38;
+	printf("%d\n",a=b);
+	printf("%d\n",b=c);
+	printf("%d\n",c=a);
+}
---
+++ Chapter-4/LEBWORK-4/5.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main(){
+	int a=325, b=256;
+	printf("%d\n", a=a+b);
+	printf("%d\n", b=a-b);
+	
+}
